934272e2e3a0b8ff59aacf4739abac927961fdf2
Mayra <mayra203@gmail.com>
2024-03-05 05:26:17 -0600

    Created home component

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent{
+  
+}
