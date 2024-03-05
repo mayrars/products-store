7aafc7b36ac4a144e85018234c67881341add45a
Mayra <mayra203@gmail.com>
2024-03-05 05:30:24 -0600

    Created routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductsDetailComponent } from './pages/products-detail/products-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:HomeComponent},
+    {path:"products",component: ProductsComponent},
+    {path:"products/:id",component:ProductsDetailComponent}
+];
