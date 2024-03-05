655068fcbf1a7efc58293c44c28064a9387a6d37
Mayra <mayra203@gmail.com>
2024-03-05 06:13:44 -0600

    Injecting service, router and adding product details in the html with the information

--- src/app/pages/products-detail/products-detail.component.ts
+++ src/app/pages/products-detail/products-detail.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../models/product.model';
+import { CurrencyPipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-products-detail',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, UpperCasePipe],
   templateUrl: './products-detail.component.html',
   styleUrl: './products-detail.component.scss'
 })
-export class ProductsDetailComponent {
-
+export class ProductsDetailComponent implements OnInit {
+  product?:Product
+  private _apiService = inject(ApiService)
+  private _route = inject(ActivatedRoute)
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      this._apiService.getProductById(params['id']).subscribe((data:Product) => {
+        this.product = data
+      })
+    })
+  }
 }
