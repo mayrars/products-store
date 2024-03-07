6993276737a1682c0d70079b964d479f7a3b991e
Mayra <mayra203@gmail.com>
2024-03-07 06:57:49 -0600

    Created slider and get all categories

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { TitleCasePipe} from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent{
-  
+export class HomeComponent implements OnInit{
+  categoriesList:string[] =[] 
+  private _apiService = inject(ApiService);
+  ngOnInit(): void {
+    this._apiService.getAllCategories().subscribe(data=>{
+      this.categoriesList = data
+    })
+  }
 }
