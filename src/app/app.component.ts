a44314a70fc43d3b4dc09b4c6a114e3c5d56c0d6
Mayra <mayra203@gmail.com>
2024-03-07 06:59:23 -0600

    Implements oninit with all categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api.service';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,RouterLink],
+  imports: [CommonModule,RouterOutlet,RouterLink,TitleCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  categoriesList:string[] = []
+  private _apiService = inject(ApiService);
   menuOption: string=''
   onOption(menuOption:string){
     this.menuOption = menuOption;
+  }
+  ngOnInit(): void {
+    this._apiService.getAllCategories().subscribe(data=>{
+      this.categoriesList = data;
+    })
   }
 }
