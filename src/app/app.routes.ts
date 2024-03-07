3af93715f770e0e48f370fb4e9d925c53c32549d
Mayra <mayra203@gmail.com>
2024-03-07 07:00:05 -0600

    Created new routes in the navigate and created routes in array routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ProductsDetailComponent } from './pages/products-detail/products-detail.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CategoryDetailComponent } from './pages/category-detail/category-detail.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
     {path:"products",component: ProductsComponent},
-    {path:"products/:id",component:ProductsDetailComponent}
+    {path:"products/:id",component:ProductsDetailComponent},
+    {path:"categories",component:CategoriesComponent},
+    {path:"categories/:name",component:CategoryDetailComponent},
 ];
