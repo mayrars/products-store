0c2cebabe3d514979d1244ed262ea194b4f08843
Mayra <mayra203@gmail.com>
2024-03-08 07:01:37 -0600

    removing header methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { ApiService } from './services/api.service';
 import { TitleCasePipe } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,RouterLink,TitleCasePipe],
+  imports: [CommonModule,RouterOutlet,RouterLink,TitleCasePipe, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
-  categoriesList:string[] = []
-  private _apiService = inject(ApiService);
-  menuOption: string=''
-  onOption(menuOption:string){
-    this.menuOption = menuOption;
-  }
-  ngOnInit(): void {
-    this._apiService.getAllCategories().subscribe(data=>{
-      this.categoriesList = data;
-    })
-  }
+export class AppComponent{
+  
 }
