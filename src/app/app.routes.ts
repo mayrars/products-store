f07e687ccb5c4a8349fb2dc42459a8062807aa86
Mayra <mayra203@gmail.com>
2024-03-13 06:05:41 -0600

    Created new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ProductsComponent } from './pages/products/products.component';
 import { ProductsDetailComponent } from './pages/products-detail/products-detail.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { CategoryDetailComponent } from './pages/category-detail/category-detail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
@@ -11,4 +13,7 @@ export const routes: Routes = [
     {path:"products/:id",component:ProductsDetailComponent},
     {path:"categories",component:CategoriesComponent},
     {path:"categories/:name",component:CategoryDetailComponent},
+    {path:"login",component:LoginComponent},
+    {path:"cart",component:CartComponent}
+
 ];
