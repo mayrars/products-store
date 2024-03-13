501e5ab3150921fdf0b9d700e37ffd5a48868989
Mayra <mayra203@gmail.com>
2024-03-13 05:16:32 -0600

    Modified categories component

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { TitleCasePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe, RouterLink],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
-export class CategoriesComponent {
-
+export class CategoriesComponent implements OnInit {
+  categoriesList: string[] = [];
+  private _apiService = inject(ApiService);
+  ngOnInit(): void {
+    this._apiService.getAllCategories().subscribe(data => {
+      this.categoriesList = data;
+    })
+  }
 }
