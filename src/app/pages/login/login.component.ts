bf0d26319ed64343879a31562921e207fe27931a
Mayra <mayra203@gmail.com>
2024-03-14 06:55:09 -0600

    implements form for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, ReactiveFormsModule,NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm!: FormGroup
+  private _apiService = inject(ApiService);
 
+  constructor(private formBuilder: FormBuilder){
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+  ngOnInit(): void {
+      
+  }
+  hasErrors(controlName: string,errorType: string){
+    return this.loginForm.get(controlName)?.hasError(errorType) && this.loginForm.get(controlName)?.touched
+  }
+  login(event: Event){
+    event.preventDefault()
+    const user = {
+      "username": this.loginForm.get('username')?.value, 
+      "password": this.loginForm.get('password')?.value
+    }
+    this._apiService.login(user).subscribe(res=>{
+      console.log(res)
+    },error=>{
+      console.error('Error al iniciar sesión:', error);
+    })
+  }
+  
 }
