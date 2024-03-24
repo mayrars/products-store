f80e414f3ee008c4a2aca7dad702abf2c9eba48d
Mayra <mayra203@gmail.com>
2024-03-23 20:09:14 -0600

    Change provide gydratation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,17 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
-    provideHttpClient(),
-    provideClientHydration()
+    provideClientHydration(
+      withHttpTransferCacheOptions({
+        includePostRequests: true
+      })
+    ),
+    provideHttpClient(withFetch()),
   ]
 };
