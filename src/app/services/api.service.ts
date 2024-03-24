5322d3d225e97948712499998e2e77876a0f61d4
Mayra <mayra203@gmail.com>
2024-03-23 20:10:13 -0600

    Change api service xD

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Product } from '../models/product.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,9 @@ import { Product } from '../models/product.model';
 export class ApiService {
   private _http = inject(HttpClient);
   private baseurl:string = "https://fakestoreapi.com"
+  currentUser: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  currentUserToken: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  
 
   getAllProducts(limit?: number):Observable<Product[]> {
     const params = limit ?  `?limit=${limit}` : '';
@@ -28,11 +32,30 @@ export class ApiService {
     return this._http.get<any>(`${this.baseurl}/products/category/${category}${params}`)
   }
   login(user:{}){
-    const params = {
-      method:"POST",
-      contentType: 'application/json',
-      body:user
-    }
-    return this._http.post(`${this.baseurl}/auth/login`,params)
+    const nuser = JSON.stringify(user)
+    const httpOptions = {
+      headers: new HttpHeaders().set('Content-Type', 'application/json').set('method','POST')
+    };
+    return this._http.post(`${this.baseurl}/auth/login`,user, httpOptions).pipe(
+      tap((userData:any) =>{
+        this.currentUserToken.next(userData)
+        this.currentUser.next(true) 
+      })
+    )
+  }
+  logOut(){
+    localStorage.removeItem('token')
+    this.currentUserToken.next('')
+    this.currentUser.next(false)
+  }
+  isAuthenticated():boolean{    
+    let token = this.currentUser
+    return token!=null || token!='' ? true : false
+  }
+  get userData():Observable<string> {
+    return this.currentUserToken.asObservable();
+  }
+  get userLoginOn():Observable<boolean> {
+    return this.currentUser.asObservable();
   }
 }
