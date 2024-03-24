6fac1186234b45a53e2146a6018bd22200a10a55
Mayra <mayra203@gmail.com>
2024-03-23 20:11:28 -0600

    Changed how to get product list

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -5,23 +5,18 @@ import { Router } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
 import { SkeletonComponent } from '../../components/skeleton/skeleton.component';
-
+import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CurrencyPipe, ProductCardComponent,SkeletonComponent],
+  imports: [CurrencyPipe, ProductCardComponent,SkeletonComponent, AsyncPipe],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent implements OnInit {
-  productsList:Product[] = [];
+export class ProductsComponent {
   private _apiService = inject(ApiService);
   private _router = inject(Router);
+  productsList = this._apiService.getAllProducts()
   constructor() { }
-  ngOnInit(): void {
-    this._apiService.getAllProducts().subscribe((data:Product[])=>{
-      this.productsList = data;
-    })
-  }
   
 }
