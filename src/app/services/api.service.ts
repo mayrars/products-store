44e75377ba998c2a46d4725247db3f5a587bb37a
Mayra <mayra203@gmail.com>
2024-06-25 05:32:30 -0600

    adding model in service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { Product } from '../models/product.model';
 import { BehaviorSubject } from 'rxjs';
+import { Login } from '../models/login.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class ApiService {
   private baseurl:string = "https://fakestoreapi.com"
   currentUser: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   currentUserToken: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  isloggedIn: boolean = false
   
 
   getAllProducts(limit?: number):Observable<Product[]> {
@@ -31,26 +33,26 @@ export class ApiService {
     const params = sort ? `?sort=${sort}` : '';
     return this._http.get<any>(`${this.baseurl}/products/category/${category}${params}`)
   }
-  login(user:{}){
-    const nuser = JSON.stringify(user)
+  login(user:Login){
     const httpOptions = {
       headers: new HttpHeaders().set('Content-Type', 'application/json').set('method','POST')
     };
     return this._http.post(`${this.baseurl}/auth/login`,user, httpOptions).pipe(
       tap((userData:any) =>{
-        this.currentUserToken.next(userData)
+        this.isloggedIn = true
+        this.currentUserToken.next(userData.token)
         this.currentUser.next(true) 
       })
     )
   }
   logOut(){
-    localStorage.removeItem('token')
     this.currentUserToken.next('')
     this.currentUser.next(false)
   }
   isAuthenticated():boolean{    
-    let token = this.currentUser
-    return token!=null || token!='' ? true : false
+    let token = this.currentUserToken
+    console.log(token)
+    return token.value!='' ? true : false
   }
   get userData():Observable<string> {
     return this.currentUserToken.asObservable();
