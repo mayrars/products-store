53bfd4f58a9364581ab4d2c71d88f9c8c837d45b
mayrars <mayra203@gmail.com>
2024-09-09 05:48:57 -0600

    change model in cart

--- src/app/models/carts.model.ts
+++ src/app/models/carts.model.ts
@@ -1,11 +1,15 @@
-export interface Carts{
-  id:Number,
-  userId:Number,
-  date:Date,
-  products:[
-    {
-      productId:Number,
-      quantity:Number
-    }
-  ]
+import { Product } from "./product.model";
+export interface Cart {
+  id:       number;
+  userId:   number;
+  date:     Date;
+  products: ProductData[];
+  __v:      number;
 }
+
+export interface ProductData {
+  productId: number;
+  ProductInfo:Product;
+  quantity:  number;
+}
+
